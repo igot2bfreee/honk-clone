1e46a32cd8b1dda80c57851b31331be18fb819b8
Jack Chen <jyc.misc@outlook.com>
2020-08-28 16:46:37 -0700

    error and extras added under /shared

---
+++ src/shared/errors.js
@@ -0,0 +1,7 @@
+export const ERRORS = [
+    {
+        id = 0,
+        text = "UH OH! We're sorry! Battery Replacements are not available in your area at this time.",
+        type = "battery"
+    },
+];
