e3a069a02e46eb832df69b857c062df41d4b1a3a
Gina <hello@ginaperano.com>
2020-08-28 18:39:47 -0700

    logo component

---
+++ src/Components/LogoComponent/LogoComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import honklogo from './HonkLogo.png';
+import './Logo.css';
+
+const Logo = () => {
+    return (
+        <div className="logo-box">
+            <img id="honklogo" alt='logo' src={honk} />
+        </div>
+    );
+}
+
+export default Logo;
+
+
+
+
